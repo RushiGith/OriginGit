e29a75f7a4648167b0985990ae04dc0dc2848544
rushi <rushikeshgomase2@gmail.com>
2023-07-19 20:23:08 +0530

    First commit

---
+++ src/com/velocity/git/Reverse.java
@@ -0,0 +1,12 @@
+package com.velocity.git;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+	  String str="Rushikesh gomase";
+	  StringBuffer b=new StringBuffer(str);
+	  System.out.println(b.reverse()); 
+
+	}
+
+}
